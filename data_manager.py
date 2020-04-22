a9c4c7c088b0fa0f3400e816d97109583cd64dcf
bezdomnij <bajni37@gmail.com>
2020-04-22 18:21:11 +0200

    Initial commit

---
+++ data_manager.py
@@ -0,0 +1,166 @@
+from typing import List, Dict
+
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
+import database_common
+
+
+@database_common.connection_handler
+def get_questions(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT *
+        FROM question
+        ORDER BY submission_time"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_vote_number(cursor: RealDictCursor, q_id:int) -> list:
+    query = """
+        SELECT vote_number
+        FROM question
+        WHERE id = %(q_id)s"""
+    cursor.execute(query, {'q_id': q_id})
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def write_vote_number(cursor: RealDictCursor, q_id:int, v_number) -> list:
+    query = """
+        UPDATE question SET vote_number = %(vote_number)s
+        WHERE id = %(q_id)s"""
+    cursor.execute(query, {'vote_number': v_number, 'q_id': q_id})
+
+
+@database_common.connection_handler
+def get_question_by_id(cursor: RealDictCursor, question_id):
+    query = """
+        SELECT * FROM question WHERE id = %(qid)s;"""
+    cursor.execute(query, {'qid': question_id})
+    return cursor.fetchone()
+
+
+@database_common.connection_handler
+def get_answers(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT *
+        FROM answer
+        ORDER BY id;"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_answer_by_question_id(cursor: RealDictCursor, question_id):
+    query = """
+        SELECT * FROM answer WHERE question_id = %(qid)s;"""
+    cursor.execute(query, {'qid': question_id})
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def add_answer(cursor: RealDictCursor, answer_details):
+    query = """
+        INSERT INTO answer  (submission_time, vote_number, question_id, message)
+        VALUES(%(s_t)s, %(v_n)s, %(q_i)s, %(m_g)s);"""
+    cursor.execute(query, {
+        's_t': answer_details['submission_time'],
+        'v_n': answer_details['vote_number'],
+        'q_i': answer_details['question_id'],
+        'm_g': answer_details['message']
+    })
+    return
+
+
+@database_common.connection_handler
+def delete_question(cursor: RealDictCursor, qid: int):
+    query = """
+        DELETE FROM question WHERE id = %(qid)s;"""
+    cursor.execute(query, {'qid': qid})
+    return
+
+
+@database_common.connection_handler
+def delete_answer(cursor: RealDictCursor, question_id: int):
+    query = """
+        DELETE FROM answer WHERE question_id = %(q_id)s;"""
+    cursor.execute(query, {'q_id': question_id})
+    return
+
+
+@database_common.connection_handler
+def update_question(cursor: RealDictCursor, question_id: int, message: str, title: str):
+    query = """
+        UPDATE question 
+        SET title = %(tt)s, message = %(msg)s
+        WHERE id = %(qid)s;"""
+    cursor.execute(query, {'tt': title, 'msg': message, 'qid': question_id})
+    return
+
+
+@database_common.connection_handler
+def insert_question(cursor: RealDictCursor, question: dict):
+    query = """
+        INSERT INTO question (submission_time, title, message, vote_number, view_number)
+        VALUES (%(st)s, %(ttl)s, %(msg)s, %(vo_n)s, %(vi_n)s);"""
+    cursor.execute(query, {
+        'st': question['submission_time'],
+        'msg': question['message'],
+        'ttl': question['title'],
+        'vo_n': question['vote_number'],
+        'vi_n': question['view_number']
+    })
+    return
+
+
+@database_common.connection_handler
+def delete_answer_by_id(cursor: RealDictCursor, answer_id: int):
+    query = """
+        DELETE FROM answer WHERE id = %(aid)s;"""
+    cursor.execute(query, {'aid': answer_id})
+    return
+
+
+@database_common.connection_handler
+def write_comment_to_question(cursor: RealDictCursor, q_id, s_time, ct):
+    query = """
+    INSERT INTO comment(question_id, submission_time, message)
+    VALUES (%(q_id)s, %(s_time)s, %(ct)s);"""
+    cursor.execute(query, {'q_id': q_id, 's_time': s_time, 'ct': ct})
+
+@database_common.connection_handler
+def get_question_comments(cursor: RealDictCursor, qid) -> list:
+    query = """
+        SELECT *
+        FROM comment WHERE question_id = %(qid)s
+        ORDER BY submission_time"""
+    cursor.execute(query, {'qid': qid})
+    return cursor.fetchall()
+
+@database_common.connection_handler
+def get_answer_comments(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT *
+        FROM comment WHERE answer_id is not null
+        ORDER BY submission_time"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def write_comment_to_answer(cursor: RealDictCursor, a_id, s_time, ct):
+    query = """
+    INSERT INTO comment(answer_id, submission_time, message)
+    VALUES (%(a_id)s, %(s_time)s, %(ct)s);"""
+    cursor.execute(query, {'a_id': a_id, 's_time': s_time, 'ct': ct})
+
+
+@database_common.connection_handler
+def get_latest_questions(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT *
+        FROM question
+        ORDER BY submission_time DESC LIMIT 5"""
+    cursor.execute(query)
+    return cursor.fetchall()
---
+++ server.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template, url_for, request, redirect
+import data_handler
+import data_manager
+from collections import OrderedDict
+from datetime import datetime
+
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_five():
+    latest_questions = data_manager.get_latest_questions()
+    # all_questions_reversed = data_handler.sorting(all_questions, sortkey='id', rev=True)
+    return render_template("landing.html", all_data_reversed=latest_questions)
+
+
+@app.route("/list")
+def get_question_list():
+    all_questions = data_manager.get_questions()
+    all_questions_reversed = data_handler.sorting(all_questions, sortkey='id', rev=True)
+    return render_template("list.html", all_data_reversed=all_questions_reversed)
+
+
+@app.route("/list/<question_id>", methods=['GET', 'POST'])
+def q_id(question_id):
+    question = data_manager.get_question_by_id(question_id)
+    if request.method == 'GET':
+        message = question['message']
+        title = question['title']
+        question_id = question['id']
+        answers = data_manager.get_answer_by_question_id(question_id)
+        if answers:
+            answer_id = answers[0]['id']
+        else:
+            answer_id = 0
+        print(answer_id)
+        comments_questions = data_manager.get_question_comments(question_id)
+        comments_answers = data_manager.get_answer_comments()
+        print(comments_answers)
+        return render_template("question_id.html", message=message, title=title, answers=answers,
+                               question_id=question_id, comments_questions=comments_questions,
+                               comments_answers=comments_answers, answer_id=answer_id)
+    elif request.method == 'POST':
+        if request.form["btn"] == "Send answer":
+            answer = OrderedDict()
+            answer['submission_time'] = datetime.now()
+            answer['vote_number'] =	0
+            answer['question_id'] = question_id
+            answer['message'] = request.form.get('comment')
+            answer['image'] = None
+            data_manager.add_answer(answer)
+            return redirect(url_for('get_question_list'))
+        elif request.form['btn'] == "Delete question":
+            data_manager.delete_question(question_id)
+            data_manager.delete_answer(question_id)
+            return redirect(url_for('get_question_list'))
+        elif request.form['btn'] == "Edit question":
+            return redirect(url_for('edit', question_id=question_id))
+
+
+@app.route('/answer/<answer_id>/delete', methods=['POST'])
+def delete_answer(answer_id):
+    question_id = request.form.get('question_id')
+    data_manager.delete_answer_by_id(answer_id)
+    return redirect(url_for('q_id', question_id=question_id))
+
+
+@app.route('/<question_id>/edit', methods=['GET', 'POST'])
+def edit(question_id):
+    question = data_manager.get_question_by_id(question_id)
+    if request.method == 'GET':
+        message = question['message']
+        title = question['title']
+        return render_template('edit.html', message=message, title=title)
+    elif request.method == 'POST':
+        title = request.form.get('title')
+        message = request.form.get('message')
+        data_manager.update_question(question_id, message, title)
+        return redirect(url_for('get_question_list'))
+
+
+@app.route("/add_question",  methods=['GET', 'POST'])
+def add_question():
+    if request.method == 'GET':
+        return render_template('add_question.html')
+    if request.method == 'POST':
+        question = {'submission_time': datetime.now(), 'view_number': 0, 'vote_number': 0,
+                    'title': request.form.get('title'), 'message': request.form.get('message'), 'image': None}
+        data_manager.insert_question(question)
+        return redirect(url_for('get_question_list'))
+
+
+@app.route("/question/<question_id>/vote_up", methods=['GET', 'POST'])
+def vote_question_up(question_id):
+    vote_number = data_manager.get_vote_number(question_id)
+    vote_number[0]['vote_number'] += 1
+    data_manager.write_vote_number(question_id, vote_number[0]['vote_number'])
+    return redirect('/list')
+
+
+@app.route("/question/<question_id>/vote_down", methods=['GET', 'POST'])
+def vote_question_down(question_id):
+    vote_number = data_manager.get_vote_number(question_id)
+    vote_number[0]['vote_number'] -= 1
+    data_manager.write_vote_number(question_id, vote_number[0]['vote_number'])
+    return redirect('/list')
+
+
+@app.route("/question/<question_id>/new-comment", methods=['GET', 'POST'])
+def add_question_comment(question_id):
+    if request.method == 'GET':
+        return render_template('add_comment.html')
+    if request.method == 'POST':
+        comment = request.form.get('comment')
+        data_manager.write_comment_to_question(question_id, datetime.now(), comment)
+    return redirect(url_for('q_id', question_id=question_id))
+
+
+@app.route("/answer/<question_id>/<answer_id>/new-comment", methods=['GET', 'POST'])
+def add_answer_comment(question_id, answer_id):
+    if request.method == 'GET':
+        return render_template('add_answer_comment.html', answer_id=answer_id)
+    if request.method == 'POST':
+        comment = request.form.get('comment')
+        data_manager.write_comment_to_answer(answer_id, datetime.now(), comment)
+        print(question_id)
+    return redirect(url_for('q_id', question_id=question_id))
+
+if __name__ == "__main__":
+    app.run(debug=True)
